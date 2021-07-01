c0f58105eaaa1dbc0c51ffdf89384771e10d1b5d
Schwibl <schwiblschwiblschwibl@gmail.com>
2021-07-01 11:08:06 +0300

    first actual html template

--- practice_site/publications/views.py
+++ practice_site/publications/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import PublicationsItem
 
 # Create your views here.
 def publications_list(request):
-  return render(request, 'publications/publications_list.html', {})
+  publications_items = PublicationsItem.objects.all().order_by('published_date')
+  return render(request, 'publications/publications_list.html', {'publications_items': publications_items})
