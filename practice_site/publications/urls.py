c30175eaba2bcbcec1337557823c13c87c4705a5
Schwibl <schwiblschwiblschwibl@gmail.com>
2021-07-08 07:22:38 +0300

    added publication page and main-menu index link

--- practice_site/publications/urls.py
+++ practice_site/publications/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.publications_list, name='publications_list'),
+    path('publications_item/<int:pk>/', views.publications_item_detail, name='publications_item')
 ] 
--- practice_site/publications/views.py
+++ practice_site/publications/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import PublicationsItem
 
 # Create your views here.
 def publications_list(request):
   publications_items = PublicationsItem.objects.all().order_by('published_date')
-  return render(request, 'publications/publications_list.html', {'publications_items': publications_items})
+  return render(request, 'pages/publications_list.html', {'publications_items': publications_items})
+
+def publications_item_detail(request, pk):
+  publications_item = get_object_or_404(PublicationsItem, pk=pk)
+  return render(request, 'pages/publications_item.html', {'publications_item': publications_item})
